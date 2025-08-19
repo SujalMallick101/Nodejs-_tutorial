c811cab3c9c009081b248c0cf51a3737648de37c
sujal mallick <mallicksujal626@gmail.com>
2025-08-19 18:27:26 +0530

    http methods

--- 06_http_methods/index.js
+++ 06_http_methods/index.js
@@ -3,19 +3,28 @@ const fs = require("fs")
 const url = require("url")
 const myServer = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") return res.end()
-    const log = `${Date.now()} : ${req.url} New req rec\n`
+    const log = `${Date.now()} : ${req.method} ${req.url} New req rec\n`
     const myUrl = url.parse(req.url, true)
-    console.log(myUrl);
 
     fs.appendFile("log.txt", log, (err) => {
         switch (myUrl.pathname) {
             case "/":
-                res.end("home page")
+                if (req.method === 'GET')
+                    res.end("home page")
                 break
 
             case "/about":
-                const userName=myUrl.query.myname
+                const userName = myUrl.query.myname
                 res.end(`hi ${userName}`)
+                break
+
+            case '/signup':
+                if (req.method === 'GET') {
+                    res.end("This is a signup form")
+                } else if (req.method === 'POST') {
+                    //DB insertion
+                    res.end("success")
+                }
                 break
 
             default:
